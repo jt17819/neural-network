2a7d04bf65dfabc8e6e18fec1f341abd73e4d19d
jt17819 <jt17819@alumni.bristol.ac.uk>
2024-12-11 15:44:43 +0000

    Added pytorch requirements

--- pytorch/app.py
+++ pytorch/app.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
+import torch
+import numpy as np
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/predict', methods=["GET", "POST"])
+def predict():
+    arr = np.array([1,2,3])
+    tensor = torch.from_numpy(arr)
+    return jsonify({"message": "Predict from Flask!", "predict": tensor.tolist()})

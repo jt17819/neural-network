6e8bb25f2efc2005ae76dfe706d021f6f49d047a
jt17819 <jt17819@alumni.bristol.ac.uk>
2026-06-21 19:52:56 +0100

    feat: added app route

---
+++ tensorflow/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import base64
+from predict import predict
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/predict', methods=["POST"])
+def predict_route():
+    data = request.get_json()
+    img = base64.b64decode(data["payload"].split(",")[1])
+    pred, output = predict(img)
+    return jsonify({"message": "Predict from Flask!", "ans": pred, "raw": output})
